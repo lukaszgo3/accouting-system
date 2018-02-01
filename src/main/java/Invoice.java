cb8d2d6fd233e28da0dd2a65044d2ad645083d8e
Krzysztof <kursantjava69@gmail.com>
2018-02-01 20:07:20 +0100

    basic project structure

--- src/main/java/Invoice.java
+++ src/main/java/Invoice.java
@@ -4,5 +4,20 @@ public class Invoice {
 
   private long id;
   private Date date;
+  private long idSeller;
+  private long idBuyer;
 
+  /**
+   *
+   * @param id unique invoice ID.
+   * @param date  invoice Data creation.
+   * @param idSeller invoice seller unique ID.
+   * @param idBuyer invoice buyer unique ID
+   */
+  public Invoice(long id, Date date, long idSeller, long idBuyer) {
+    this.id = id;
+    this.date = date;
+    this.idSeller = idSeller;
+    this.idBuyer = idBuyer;
+  }
 }
