60c21f514fb489a56a7f390028e431d003a71431
Krzysztof <kursantjava69@gmail.com>
2018-02-01 20:39:03 +0100

    basic project structure - passes checkstyle, jacoco

--- src/main/java/domain/Invoice.java
+++ src/main/java/domain/Invoice.java
@@ -1,33 +1,9 @@
 package domain;
 
-import java.util.Date;
-
 public class Invoice {
 
-  private long id;
-  private Date date;
   private long idSeller;
   private long idBuyer;
-
-  /**
-   * This is default constructor.
-   */
-  public Invoice() {
-  }
-
-  /**
-   * This is constructor.
-   * @param id unique invoice ID.
-   * @param date invoice Data creation.
-   * @param idSeller invoice seller unique ID.
-   * @param idBuyer invoice buyer unique ID
-   */
-  public Invoice(long id, Date date, long idSeller, long idBuyer) {
-    this.id = id;
-    this.date = date;
-    this.idSeller = idSeller;
-    this.idBuyer = idBuyer;
-  }
 
   /**
    * Method returns sum.
