5f194457ca43fced94e75d9bf1c30fc86a851f3c
SebastianPal <spalewski@gmail.com>
2018-02-11 16:18:22 +0100

    MultiFileDatabase

--- src/main/java/pl/coderstrust/database/multifile/MultiFileDatabase.java
+++ src/main/java/pl/coderstrust/database/multifile/MultiFileDatabase.java
@@ -1,14 +1,28 @@
 package pl.coderstrust.database.multifile;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import pl.coderstrust.database.Database;
 import pl.coderstrust.model.Invoice;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class MultiFileDatabase implements Database {
 
+  ObjectMapper objectMapper = new ObjectMapper();
+  PathSelector pathSelector;
+  Invoice invoice;
+  String filePath;
+
   @Override
   public void addInvoice(Invoice invoice) {
+    try {
+      objectMapper.writeValue(new File(pathSelector.getFilePath(invoice)), invoice);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
 
   }
 
---
+++ src/main/java/pl/coderstrust/database/multifile/PathSelector.java
@@ -0,0 +1,39 @@
+package pl.coderstrust.database.multifile;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import pl.coderstrust.model.Invoice;
+
+
+public class PathSelector {
+
+  ObjectMapper objectMapper = new ObjectMapper();
+  StringBuilder stringBuilder = new StringBuilder();
+
+  String filePath = "";
+
+  public PathSelector(String filePath) {
+    this.filePath = filePath;
+  }
+
+  /**
+   * PathSelector gives specific path of each invoice to write and read
+   */
+
+  public String getFilePath(Invoice invoice) {
+    String invoiceDateYear = String.valueOf(invoice.getIssueDate().getDayOfYear());
+    String invoiceDateMonth = String.valueOf(invoice.getIssueDate().getDayOfYear());
+    String invoiceDateDay = String.valueOf(invoice.getIssueDate().getDayOfYear());
+
+    stringBuilder.append(invoiceDateYear);
+    stringBuilder.append("\\");
+    stringBuilder.append(invoiceDateMonth);
+    stringBuilder.append("\\");
+    stringBuilder.append(invoiceDateDay);
+    stringBuilder.append("\\");
+    stringBuilder.append(invoiceDateYear + invoiceDateMonth + invoiceDateDay + ".json");
+
+    filePath = stringBuilder.toString();
+    return filePath;
+  }
+}
