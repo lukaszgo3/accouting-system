9e3f905f7d245c32ac9842e463ec54985c785332
SebastianPal <spalewski@gmail.com>
2018-02-18 21:50:56 +0100

    ExceptionMSG added

--- src/main/java/pl/coderstrust/database/multifile/DbException.java
+++ src/main/java/pl/coderstrust/database/multifile/DbException.java
@@ -1,6 +1,8 @@
 package pl.coderstrust.database.multifile;
 
-public class DbException extends Exception {
+public class DbException extends RuntimeException {
 
-
+  public DbException(String message) {
+    super(message);
+  }
 }
