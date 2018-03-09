492c32698ae594aa7e41efeb4d8993561e03a354
lukaszgo3 <lukaszgo3@gmail.com>
2018-03-09 13:52:53 +0100

    some fixes

--- src/main/java/pl/coderstrust/service/InvoiceBookController.java
+++ src/main/java/pl/coderstrust/service/InvoiceBookController.java
@@ -1,6 +1,5 @@
 package pl.coderstrust.service;
 
-import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
