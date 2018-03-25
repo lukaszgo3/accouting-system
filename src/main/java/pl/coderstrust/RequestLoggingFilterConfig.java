00944aa2c7d6a5aa5a7a02285a5ae340b333e924
lukaszgo3 <lukaszgo3@gmail.com>
2018-03-25 14:20:15 +0200

    ServletRegistrationBean

--- src/main/java/pl/coderstrust/RequestLoggingFilterConfig.java
+++ src/main/java/pl/coderstrust/RequestLoggingFilterConfig.java
@@ -1,8 +1,11 @@
 package pl.coderstrust;
 
+import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
 public class RequestLoggingFilterConfig {
@@ -13,8 +16,18 @@ public class RequestLoggingFilterConfig {
     filter.setIncludeQueryString(true);
     filter.setIncludePayload(true);
     filter.setMaxPayloadLength(10000);
-    filter.setIncludeHeaders(true);
+    filter.setIncludeHeaders(false);
     filter.setAfterMessagePrefix("REQUEST DATA: ");
     return filter;
+  }
+
+  @Bean
+  public ServletRegistrationBean dispatcherRegistration() {
+    return new ServletRegistrationBean(dispatcherServlet());
+  }
+
+  @Bean(name = DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_BEAN_NAME)
+  public DispatcherServlet dispatcherServlet() {
+    return new LoggableDispatcherServlet();
   }
 }
