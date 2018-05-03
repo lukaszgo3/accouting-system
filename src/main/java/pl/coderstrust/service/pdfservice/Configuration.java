c93ba4959c0de9b518b09b4a794cf0a9ebb44ccc
kmalysiakPL <kursantjava69@gmail.com>
2018-05-04 01:10:02 +0200

    pdf service is working

--- src/main/java/pl/coderstrust/service/pdfservice/Configuration.java
+++ src/main/java/pl/coderstrust/service/pdfservice/Configuration.java
@@ -2,14 +2,9 @@ package pl.coderstrust.service.pdfservice;
 
 import java.math.BigDecimal;
 
-;
-
 public class Configuration {
-
-
   public static final float DEFAULT_MARGIN_SIZE = 20;
   public static final String DATE_FORMAT = "yyyy/MM/dd HH:mm:ss";
-  //public static final String TEMP_FILE_NAME = "iTextTable.pdf";
   public static final int ROUND_DIGITS_BIG_DECIMAL = 4;
   public static final int ROUND_MODE_BIG_DECIMAL = BigDecimal.ROUND_HALF_UP;
   public static final int COMPANIES_TABLE_COLUMNS_COUNT = 3;
@@ -18,6 +13,4 @@ public class Configuration {
   protected static final String BASE_FONT_NAME = "DejaVuSans.ttf";
   protected static final int DEFAULT_FONT_SIZE = 12;
   protected static final int HEADER_DEFAULT_FONT_SIZE = 20;
-
-
 }
